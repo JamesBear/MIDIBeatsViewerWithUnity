2fae4c409dd405510584d5201c0d25d2c0a4b0d7
JamesBear <jamesgrizzly@gmail.com>
2016-11-17 18:04:59 +0800

    set panel width

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,14 +2,33 @@
 using System.Collections;
 using NAudio.Midi;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class Test : MonoBehaviour {
 
+    class Beat
+    {
+        public NoteOnEvent _event;
+        public int trackIndex;
+        public int Time
+        {
+            get
+            {
+                return (int)_event.AbsoluteTime;
+            }
+        }
+    }
+
     public Text console;
     public RectTransform grid;
+    public GameObject buttonPrefab;
+    public Scrollbar scrollBar;
 
-    float ticksPerMinute = 1f;
+    float ticksPerSecond = 1f;
     private float musicLength = 1f;
+    MidiFile midiFile;
+    List<Beat> beats = new List<Beat>();
+    
 
 	// Use this for initialization
 	void Start () {
@@ -29,13 +48,14 @@ public class Test : MonoBehaviour {
     void LoadMidiFile(string fileName)
     {
         MidiFile mf = new MidiFile(fileName);
-        var ev = mf.Events[0][0];
         long highestTick = -1;
         int bpm = 120;
         int tempo_count = 0;
+        beats.Clear();
         
-        foreach (var channel in mf.Events)
+        for (int i = 0; i < mf.Events.Tracks; i ++)
         {
+            var channel = mf.Events[i];
             foreach (var eve in channel)
             {
                 highestTick = (eve.AbsoluteTime > highestTick) ? eve.AbsoluteTime : highestTick;
@@ -55,15 +75,33 @@ public class Test : MonoBehaviour {
                 }
                 else if (eve.CommandCode == MidiCommandCode.NoteOn)
                 {
-
+                    NoteOnEvent noteOnEvent = eve as NoteOnEvent;
+                    Beat beat = new Beat();
+                    beat._event = noteOnEvent;
+                    beat.trackIndex = i;
+                    beats.Add(beat);
                 }
             }
         }
 
-        ticksPerMinute = bpm * mf.DeltaTicksPerQuarterNote;
-        musicLength = ((float)highestTick / ticksPerMinute) * 60;
+        ticksPerSecond = bpm * mf.DeltaTicksPerQuarterNote / 60f;
+        musicLength = highestTick / ticksPerSecond;
 
         Debug.Log(string.Format("music length = {0}, bpm = {1}", musicLength, bpm));
+        beats.Sort((a, b) => a._event.AbsoluteTime.CompareTo(b._event.AbsoluteTime));
+        Debug.Log(string.Format("beat count = {0}, last beat time = {1}, last beat = {2}", 
+            beats.Count, beats[beats.Count-1]._event.AbsoluteTime / ticksPerSecond,
+            beats[beats.Count - 1]._event));
+    }
+
+    void SetupUI()
+    {
+        var size = grid.sizeDelta;
+        size.x = beats[beats.Count - 1].Time;
+        grid.sizeDelta = size;
+        scrollBar.value = 0;
+
+        
     }
 
     public void Load()
@@ -73,5 +111,6 @@ public class Test : MonoBehaviour {
         //AudioFileInspector.MidiFileInspector mf = new AudioFileInspector.MidiFileInspector();
         string fileName = AskForFileName();
         LoadMidiFile(fileName);
+        SetupUI();
     }
 }
